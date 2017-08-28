e31243c3689520b6c58d48392e9832f16099a587
Carl Luo <cluo@jobs2careers.com>
2017-08-28 16:56:22 -0500

    post is good

--- src/api/CourseApi.js
+++ src/api/CourseApi.js
@@ -2,6 +2,7 @@ import delay from './delay';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:9000';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
@@ -53,11 +54,6 @@ function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
 
-//This would be performed on the server in a real app. Just stubbing in.
-const generateId = (course) => {
-    return replaceAll(course.title, ' ', '-');
-};
-
 class CourseApi {
     static getAllCourses() {
         return axios.get('/courses');
@@ -65,29 +61,15 @@ class CourseApi {
 
     static saveCourse(course) {
         course = Object.assign({}, course); // to avoid manipulating object passed in.
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // Simulate server-side validation
-                const minCourseTitleLength = 1;
-                if (course.title.length < minCourseTitleLength) {
-                    reject(`Title must be at least ${minCourseTitleLength} characters.`);
-                }
-
-                if (course.id) {
-                    const existingCourseIndex = courses.findIndex(a => a.id === course.id);
-                    courses.splice(existingCourseIndex, 1, course);
-                } else {
-                    //Just simulating creation here.
-                    //The server would generate ids and watchHref's for new courses in a real app.
-                    //Cloning so copy returned is passed by value rather than by reference.
-                    course.id = generateId(course);
-                    course.watchHref = `http://www.pluralsight.com/courses/${course.id}`;
-                    courses.push(course);
-                }
-
-                resolve(course);
-            }, delay);
-        });
+        const json = "{\n" +
+            "    \"id\": \"react-flux-building-applications\",\n" +
+            "    \"title\": \"Building Applications in React and Flux\",\n" +
+            "    \"watchHref\": \"http://www.pluralsight.com/courses/react-flux-building-applications\",\n" +
+            "    \"authorId\": \"cory-house\",\n" +
+            "    \"length\": \"5:08\",\n" +
+            "    \"category\": \"JavaScript\"\n" +
+            "}";
+        return axios.post('/courses', json);
     }
 
     static deleteCourse(courseId) {
